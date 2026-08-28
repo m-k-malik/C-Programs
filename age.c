dfe26cf34f3cff4f410b2f279284db24b0a6e874
M. K. Malik <mohdkaifmalik333@gmail.com>
2026-08-28 21:09:35 +0530

    added new file

---
+++ age.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if (age<18 && age>12){
+        printf("You are teenager.");
+    }
+    else if (age>=18){
+        printf("You are adult.");
+    }
+    else{
+        printf("You are not adult.");
+    }
+
+    return 0;
+}
